66433b45b697707079589d1253fdf032d5e517de
povarna <vlad.povarna@uipath.com>
2024-05-28 23:09:21 +0300

    WIP

--- exercises/06_ticket_management/07_combinators/src/lib.rs
+++ exercises/06_ticket_management/07_combinators/src/lib.rs
@@ -31,6 +31,12 @@ impl TicketStore {
     pub fn add_ticket(&mut self, ticket: Ticket) {
         self.tickets.push(ticket);
     }
+
+    pub fn to_dos(&mut self) ->  Vec<&Ticket> {
+        self.tickets.iter()
+        .filter(|ticket| ticket.status == Status::ToDo)
+        .collect::<Vec<&Ticket>>()
+    }
 }
 
 #[cfg(test)]
