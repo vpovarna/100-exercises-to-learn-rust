68728e7069f842642d1c5b681e77f86754913eaf
povarna <vlad.povarna@uipath.com>
2024-05-28 23:19:11 +0300

    streaming folder

---
+++ exercises/09_streaming/strsplit/src/lib.rs
@@ -0,0 +1,11 @@
+//!
+#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+
+pub struct StrSplit {
+
+}
+
+impl  StrSplit {
+    pub fn new(haystack: &str, delimiter: &str) -> Self {}
+}
+
